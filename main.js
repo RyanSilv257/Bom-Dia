37cd2dda2ff4f33f7f3b876836114a3078a2d7a1
RyanSilv257 <130668947+RyanSilv257@users.noreply.github.com>
2023-08-11 16:01:09 -0300

    coasdkasj

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
-let saudacao = "Bom dia!"
+let hora = new Date().getHours();
+let saudacao = ""
+
+if ( hora >= 6 && hora <= 12) {
+    saudacao = "Bom Dia!"
+}
+
+else if (hora >= 13 && hora <= 18) {
+    saudacao = "Boa Tarde!"
+}
+
+else {
+    saudacao = "Boa Noite!"
+}
 
 console.log(saudacao)
 
